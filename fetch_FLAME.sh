9029d5c097cb0f47d93d012f6ce66bb735253502
tbolkart <tbolkart@ps107.is.localnet>
2022-07-07 15:01:26 +0200

    add automatic FLAME downloading

---
+++ fetch_FLAME.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+
+# Fetch FLAME data
+echo -e "\nBefore you continue, you must register at https://flame.is.tue.mpg.de/ and agree to the FLAME license terms."
+read -p "Username (FLAME):" username
+read -p "Password (FLAME):" password
+username=$(urle $username)
+password=$(urle $password)
+
+mkdir -p ./models
+
+echo -e "\nDownloading FLAME..."
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './models/FLAME2020.zip' --no-check-certificate --continue
+unzip ./models/FLAME2020.zip -d ./data
